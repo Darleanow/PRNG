d98942946ae11b55ba3ace3a358ab1d0411e025f
Darleanow <enzohugonn@gmail.com>
2026-06-16 12:32:43 +0200

    refactor(main): Update verbose output message and streamline LFSR PoC execution

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+
 #include <grendizer/grendizer.hpp>
 #include <prng/lfsr/lfsr.hpp>
 #include <spdlog/spdlog.h>
@@ -8,7 +10,7 @@ int main(int argc, char **argv) {
   int verbose = 0;
   int lfsr_flag = 0;
   gr_opt opts[] = {GR_COUNT('v', "verbose", &verbose,
-                            "Enable verbose output (with levels -v -vv -vvv)"),
+                            "Enable verbose output (with levels -v -vv)"),
                    GR_FLAG('l', "lfsr", &lfsr_flag, "Enable lfsr PoC"), GR_END};
   gr_spec spec = {"prng_app", "prng_app [options] <input>", opts, NULL};
   gr_rest rest;
@@ -20,25 +22,10 @@ int main(int argc, char **argv) {
   }
 
   constexpr level_enum levels[] = {warn, info, debug, trace};
-  int idx = std::clamp(verbose, 0, 3);
-  spdlog::set_level(levels[idx]);
+  spdlog::set_level(levels[std::clamp(verbose, 0, 3)]);
 
   if (lfsr_flag) {
-    spdlog::info("Beginning PoC of lfsr break with GF(2)");
-
-    LFSR victim;
-    victim.val = 0xDEADBEEF;
-    spdlog::debug("Initial state={}", victim.val);
-
-    constexpr size_t N = 32;
-    uint8_t observed[2 * N];
-
-    for (size_t n = 0; n < 2 * N; n++) {
-      unsigned out = step(victim);
-      observed[n] = out;
-      spdlog::debug("step {}: state={}, out={}", n, victim.val, out);
-    }
-    spdlog::info("Done observing !");
+    return solver::run_lfsr_poc();
   }
 
   return 0;
